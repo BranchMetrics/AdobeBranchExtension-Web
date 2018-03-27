1a9ac3f4ec4bcad980dbe127e1b441097ce4157f
Rubin Singh <rubinsingh@gmail.com>
2018-03-26 19:26:45 -0700

    removing options from view and adding setBranchViewData() call

--- src/lib/actions/autoOpen.js
+++ src/lib/actions/autoOpen.js
@@ -1,8 +1,7 @@
 'use strict';
 module.exports = function(settings) {
 	var autoOpenLink = settings['autoOpenLink'] || null;
-	var autoOpenOptions = settings['autoOpenOptions'] || null;
-	if (autoOpenLink && autoOpenOptions) {
+	if (autoOpenLink) {
 		if (autoOpenLink.data && !autoOpenLink.data.$uri_redirect_mode) {
 			// set uri redirect mode to 1 if not present in link data
 			autoOpenLink.data.$uri_redirect_mode = 1;
@@ -10,9 +9,11 @@ module.exports = function(settings) {
 		if (autoOpenLink && !autoOpenLink.channel) {
 			autoOpenLink.channel = 'mobile_web_launch';
 		}
+		branch.setBranchViewData(
+			Object.assign({}, autoOpenLink)
+		);
 		branch.deepview(
-			Object.assign({}, autoOpenLink),
-			Object.assign({}, autoOpenOptions)
+			Object.assign({}, autoOpenLink)
 		);
 	}
 };
